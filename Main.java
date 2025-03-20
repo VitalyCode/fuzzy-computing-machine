5bdedbf63702744dee7b5f7136479d03172207d3
VitalyCode <dehup182022@yandex.ru>
2025-03-20 19:22:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    // Задание №1
+    System.out.println("Vitaly Ivanov");
+    // Задание №2
+    System.out.println(""); // Отступ
+    int income = 44000; // Целочисленный тип данных: Для хранение размера доходов
+    int spending = 30000; // Целочисленный тип данных: Для хранение размера расходов
+    System.out.println("Итого (руб): ");
+    System.out.println(income - spending);
+  }
+}
